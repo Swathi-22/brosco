0058e03588ad247be6b411e187907be37cbbec97
swathi-22 <mkswathisuresh@gmail.com>
2022-05-31 17:11:49 +0530

    logo adding

--- web/views.py
+++ web/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context ={
-
+        'is_index':True
     }
     return render(request,'web/index.html',context)
+
+
+def contact(request):
+    context = {
+        'is_contact':True
+    }
+    return render(request,'web/contact.html',context)
