68ee900c233c36e556f6fe97e7e28ae9dd8923f4
swathi-22 <mkswathisuresh@gmail.com>
2022-06-06 12:19:12 +0530

    All pages settings

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,9 @@ app_name='web'
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('about/', views.about,name='about'),
+    path('services/', views.services,name='services'),
+    path('gallery/', views.gallery,name='gallery'),
+    path('update/', views.update,name='update'),
     path('contact/',views.contact,name='contact')
 ]
--- web/views.py
+++ web/views.py
@@ -9,6 +9,34 @@ def index(request):
     return render(request,'web/index.html',context)
 
 
+def about(request):
+    context ={
+        'is_about':True
+    }
+    return render(request,'web/about.html',context)
+
+
+def services(request):
+    context ={
+        'is_services':True
+    }
+    return render(request,'web/services.html',context)
+
+
+def gallery(request):
+    context ={
+        'is_gallery':True
+    }
+    return render(request,'web/gallery.html',context)
+
+
+def update(request):
+    context ={
+        'is_update':True
+    }
+    return render(request,'web/update.html',context)
+
+
 def contact(request):
     context = {
         'is_contact':True
